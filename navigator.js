66dca8c6d3dce65fa2885173c5054605a8e51e47
DarkWolf2244 <darkwolfx2244@gmail.com>
2023-05-03 15:53:35 +0530

    Navigator support

--- navigator.js
+++ navigator.js
@@ -4,10 +4,28 @@ class navigator_class {
         'how_it_works': 'how_it_works.html',
         'create_account': 'create_account.html',
         'dashboard': 'dashboard.html',
+        'main': 'main.html'
     }
 
     goto(page) {
         window.location = this.urls[page];
+    }
+
+    open() {
+        console.log("opening");
+        $('.nav-popup-container')[0].classList.remove('invisible')
+    }
+
+    close() {
+        $('.nav-popup-container')[0].classList.add('invisible')
+    }
+
+    toggle() {
+        if ($('.nav-popup-container')[0].classList.contains('invisible')) {
+            this.open();
+        } else {
+            this.close();
+        }
     }
 }
 
